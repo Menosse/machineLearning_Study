14bdf7494560303935da59c40d7c1faa47f4374d
Menosse <famcsn2@gmail.com>
2019-03-03 11:22:35 +0900

    remove tudo

--- Part 2 - Regression/Section 4 - Simple Linear Regression/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-# IMPORTING DATASET
-dataset = pd.read_csv('Salary_Data.csv')
-x = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
