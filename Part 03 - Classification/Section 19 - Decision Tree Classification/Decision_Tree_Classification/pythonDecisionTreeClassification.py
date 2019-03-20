2b960a01eefc38f728d156a939b2d38f67007a88
Menosse <famcsn2@gmail.com>
2019-03-20 09:08:42 +0900

    Creating Classification models using Decision Tree and Random Forest

---
+++ Part 03 - Classification/Section 19 - Decision Tree Classification/Decision_Tree_Classification/pythonDecisionTreeClassification.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 19:09:34 2019
+
+@author: ts-fernando.takada
+"""
+
+# Decision Tree Classification 
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+xTrain, xTest, yTrain, yTest = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+# There is no need to apply feature scaling for decision tree
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+xTrain = sc.fit_transform(xTrain)
+xTest = sc.transform(xTest)
+
+# Fitting classifier to the Training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(xTrain, yTrain)
+
+# Predicting the Test set results
+yPred = classifier.predict(xTest)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(yTest, yPred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+xSet, ySet = xTrain, yTrain
+X1, X2 = np.meshgrid(np.arange(start = xSet[:, 0].min() - 1, stop = xSet[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = xSet[:, 1].min() - 1, stop = xSet[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(ySet)):
+    plt.scatter(xSet[ySet == j, 0], xSet[ySet == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+xSet, ySet = xTest, yTest
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = xSet[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = xSet[:, 1].min() - 1, stop = xSet[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(ySet)):
+    plt.scatter(xSet[ySet == j, 0], xSet[ySet == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
