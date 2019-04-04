b63fbeb2dbf9057a6e289622a9aef40073bb0ad0
Menosse <famcsn2@gmail.com>
2019-04-04 12:01:56 +0900

    This is the commit to add reinforcement learning to my project

---
+++ Part 06 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/UCB/pythonUCB.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 07:53:48 2019
+
+@author: ts-fernando.takada
+"""
+
+# Importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Importing dataset
+
+dataset = pd.read_csv("Ads_CTR_Optimisation.csv")
